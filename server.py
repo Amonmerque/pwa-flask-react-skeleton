7266661604ac0dbca560d55f2bf51250febaffeb
Amonmerque <alexandre.monmerque@free.fr>
2019-11-03 23:56:30 +0100

    TESTS

--- server.py
+++ server.py
@@ -1,22 +1,29 @@
 from flask import Flask, render_template, send_from_directory, redirect
+from flask_socketio import SocketIO
 
 
 app = Flask(__name__, static_folder="build/static", template_folder="build")
-
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/test")
-def tel():
-    print("TEST")
-    return redirect("tel://0640865682")
-
-
 @app.route('/<path:path>')
 def get_dir(path):
     return send_from_directory('./build', path)
 
+# @socketio.on('REQUEST_MESSAGE')
+# def request_message(module):
+#     print("REQUEST_MESSAGE")
+#     emit('TO_PLATFORM', module, broadcast=True)
 
-app.run(host='192.168.1.8', port='5000', debug=True)
+@socketio.on('REQUEST_MESSAGE')
+def handle_my_custom_event(json):
+    print("RECEIVED REQUEST")
+    socketio.emit('TO_PLATFORM', json)
+
+
+# socketio.run(app, host='192.168.1.8', port='5000', debug=True)
+socketio.run(app, host='localhost', port='5000', debug=True)
--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import socketIOClient from "socket.io-client";
+
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.sendPlatformMessage = this.sendPlatformMessage.bind(this);
+    this.requestPlatformMessage = this.requestPlatformMessage.bind(this);
+  }
+
+  sendPlatformMessage(module) {
+    alert(module)
+    document.getElementById(module).reset();
+    document.getElementById(module).submit()
+  }
+
+  requestPlatformMessage = (module) => {
+    this.socket.emit('REQUEST_MESSAGE', module);
   }
 
   componentDidMount() {
+    this.socket = socketIOClient('http://localhost:5000')
+
+    this.socket.on('TO_PLATFORM', data => this.sendPlatformMessage(data));
+
     setTimeout(function(){
-    document.getElementById("FORM_WHATSAPP").submit()
-    // window.location='/test';
+      this.sendPlatformMessage("FORM_TEL")
     }, 5000);
+
+    setTimeout(function(){
+      this.sendPlatformMessage("FORM_WHATSAPP")
+    }, 15000);
   }
   
   render() {
@@ -19,13 +40,34 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a href="/test" >TEST WHATSAPP</a>
-          <form action="/test" id="FORM_WHATSAPP">
-          </form>
+          
+          {/* <button onClick={() => { this.requestPlatformMessage('FORM_TEL') }}>REQUEST TEL!</button>
+          <button onClick={() => { this.requestPlatformMessage('FORM_WHATSAPP') }}>REQUEST WHATSAPP</button>
+
+          <button onClick={() => { this.sendPlatformMessage('FORM_TEL') }}>GO TEL!</button>
+          <button onClick={() => { this.sendPlatformMessage('FORM_WHATSAPP') }}>GO WHATSAPP</button>
+
+          <form method="post" action="tel://0640865682" Title="TITLE"  target="new" id="FORM_TEL"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="FORM_WHATSAPP" Title="TITLE" target="new"></form> */}
+
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T1" Title="TITLE" target="new"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T2" target="new"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T3" target="_blank"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T4" Title="TITLE"  target="new"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T5" Title="TITLE"  target="_blank"></form>
+          <form method="post" action="whatsapp://send?phone=0640865682" id="T6"></form>
+          <button onClick={() => { this.sendPlatformMessage('T1') }}>T1</button>
+          <button onClick={() => { this.sendPlatformMessage('T2') }}>T2</button>
+          <button onClick={() => { this.sendPlatformMessage('T3') }}>T3</button>
+          <button onClick={() => { this.sendPlatformMessage('T4') }}>T4</button>
+          <button onClick={() => { this.sendPlatformMessage('T5') }}>T5</button>
+          <button onClick={() => { this.sendPlatformMessage('T6') }}>T6</button>
+          <a href="whatsapp://send?phone=0640865682"  target="_blank">Le site</a>
+
         </header>
+        {/* <script>
+          function myFunction(n,t){document.getElementById(n).submit(),window.reload()}
+        </script> */}
       </div>
     );
   }
